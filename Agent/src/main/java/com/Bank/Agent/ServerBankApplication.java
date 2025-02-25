5252f1f6513e852424bb9745fc649dd642a7bfbd
luccasilvx <lucafreitasilva@gmail.com>
2025-02-24 21:43:55 -0300

    Initial commit

---
+++ Agent/src/main/java/com/Bank/Agent/ServerBankApplication.java
@@ -0,0 +1,13 @@
+package com.Bank.Agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerBankApplication.class, args);
+	}
+
+}
---
+++ Agent/src/test/java/com/Bank/Agent/ServerBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Bank.Agent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
